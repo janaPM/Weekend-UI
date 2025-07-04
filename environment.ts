8671727ee420dcfd2e279295d8f44cfb74bbcc06
janaPM <jana@profice.co.uk>
2025-07-04 18:54:14 +0530

    url

--- environment.ts
+++ environment.ts
@@ -5,5 +5,5 @@ export const environment = {
     AJAN:'http://192.168.1.15:3000/',
     JSK:'http://192.168.1.27:3000/',
     HOST:'http://192.168.1.4:3000/',
-    URL:'/api',
+    URL:'http://3.110.209.197:4000/api',
   };
